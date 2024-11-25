a20ab3ef64408a210d1d2c8020fe2362a5e2b112
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-24 23:02:20 -0800

    add instructions of the problem, open a function that will satisfy a solution.

--- are_you_playing_banjo.py
+++ are_you_playing_banjo.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+# Create a function which answers the question "Are you playing banjo?".
+# If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+# The function takes a name as its only argument, and returns one of the following strings:
+
+# name + " plays banjo" 
+# name + " does not play banjo"
+
+my_name = "Richard"
+my_name2 = "Timothy"
+
+def are_you_playing_banjo(name):
+    
+    print("Hello World!")
+    
+are_you_playing_banjo(my_name)
