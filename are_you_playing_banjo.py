3db5acdc7ff42c9aa05e877b8072bdf96eb2355f
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-25 13:34:24 -0800

    check that a name starting with the letter 'R' or 'r' returns the proper message.

--- are_you_playing_banjo.py
+++ are_you_playing_banjo.py
@@ -10,7 +10,9 @@ my_name = "Richard"
 my_name2 = "Timothy"
 
 def are_you_playing_banjo(name):
-    
-    print("Hello World!")
+    if my_name[0] == "R" or my_name[0] == "r":
+        print(f"{my_name } plays Banjo!")
+    else:
+        print(f"{my_name} does not play banjo")
     
 are_you_playing_banjo(my_name)
